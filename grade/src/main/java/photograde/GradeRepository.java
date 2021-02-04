48bcbdc14fc4f8063bb798296ef00d4e17c10581
WonGil <wongilgom@gmail.com>
2021-02-04 13:58:16 +0900

    Delete grade directory

--- grade/src/main/java/photograde/GradeRepository.java
+++
@@ -1,8 +0,0 @@
-package photograde;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface GradeRepository extends PagingAndSortingRepository<Grade, Long>{
-
-
-}
